1c84e627a4eb028a671c80a7c3ea17a496bbaff9
vqanh77 <vqanh77@gmail.com>
2022-11-15 22:25:37 +0700

    initial commit

---
+++ controllers/main.js
@@ -0,0 +1,49 @@
+
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
+
+const login = async (req,res) => {
+    const{username,password} = req.body
+    // mongoose validation
+    // joi
+    // check in the controller
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password!',400)
+    }
+
+    const id = new Date().getDate()
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+
+    // res.send('Fake Login/ Register/ Signup Route')
+    res.status(200).json({msg: 'user created',token})
+
+    
+
+}
+
+const dashboard = async (req,res) =>{
+    const authHeader = req.headers.authorization
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token provided',401)
+    }
+    const token = authHeader.split(' ')[1]
+    console.log(token);
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log(decoded);
+        const luckyNumber = Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello ${decoded.username}`, secret: `Here is your authorized data is ${luckyNumber}`})
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route',401)
+    }
+
+    
+    
+}
+
+module.exports = {
+    login,dashboard
+}
